c121681f1f82d7fbd84f216ca596199c77568160
tyousinu1984 <tyousinu1984@yahoo.co.jp>
2020-12-24 23:39:41 +0900

    db操作

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -2,18 +2,61 @@ package dbops
 
 import (
 	"database/sql"
+	"log"
+
+	// MySQL driver
+	_ "github.com/go-sql-driver/mysql"
 )
-
-func openConn() *sql.DB {
-
-}
 
 // AddUserCredential is adding userinfo to db
 func AddUserCredential(loginName, pwd string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
 
+	_, err = stmtIns.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+
+	defer stmtIns.Close()
+
+	return nil
 }
 
 // GetUserCredential is getting userinfo from db
 func GetUserCredential(loginName string) (string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err != nil {
+		log.Printf("%s", err)
+		return "", err
+	}
 
+	var pwd string
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows {
+		return "", err
+	}
+
+	defer stmtOut.Close()
+
+	return pwd, nil
+}
+
+// DeleteUser is
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROm users WHERE login_name=? AND pwd=?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+		return err
+	}
+
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
 }
--- api/handler.go
+++ api/handler.go
@@ -7,10 +7,13 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// CreateUser is
 func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	io.WriteString(w, "Create User Handler")
 }
 
+// Login is
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	uname = 
+	uname := p.ByName("user_name")
+	io.WriteString(w, uname)
 }
